568806abc3d0e475579ef77c44743d93cf8316e8
Paavan Agrawal <33314332+AgrPaavan@users.noreply.github.com>
2022-09-30 14:04:17 +0530

    Change color for name

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -30,7 +30,7 @@ const Navbar = () => {
   return (
     <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed">
       <div>
-        <h1 className="text-5xl font-signature ml-2">Paavan</h1>
+        <h1 className="text-5xl font-signature ml-2 text-[#219be5]">Paavan</h1>
       </div>
 
       <ul className="hidden md:flex">
