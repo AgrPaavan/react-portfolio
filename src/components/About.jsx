51ed11ac0d514662ee7845d96aadca776c250350
Paavan Agrawal <33314332+AgrPaavan@users.noreply.github.com>
2022-10-02 16:31:14 +0530

    Change heading tag

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,9 +8,9 @@ const About = () => {
     >
       <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
         <div className="pb-8">
-          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+          <h2 className="text-4xl font-bold inline border-b-4 border-gray-500">
             About
-          </p>
+          </h2>
         </div>
 
         <p className="text-xl mt-20">
